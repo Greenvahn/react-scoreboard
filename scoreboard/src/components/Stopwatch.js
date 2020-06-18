a97264692450146315da88b7bf5782c80457e5f1
Saul Verde <saulverdemartinez@gmail.com>
2020-06-18 21:01:00 +0100

    reset function for the stopwatch

--- scoreboard/src/components/Stopwatch.js
+++ scoreboard/src/components/Stopwatch.js
@@ -4,33 +4,51 @@ class Stopwatch extends Component {
 
     state = {
         isRunning: false,
-      }
+        elapsedTime: 0,
+        previousTime: 0
+    }
 
     // Lifecycle method built in react
     // It triggers just after the component has been mounted into the dom    
-    componentDidMount () {
-        this.intervalID = setInterval(() => this.tick(),100);
+    componentDidMount() {
+        this.intervalID = setInterval(() => this.tick(), 100);
     }
-      
+
     tick = () => {
-        console.log('Ticking....!')
+        if (this.state.isRunning) {
+            const now = Date.now();
+            this.setState(prevState => ({
+                previousTime: now,
+                elapsedTime: prevState.elapsedTime + (now - this.state.previousTime)
+            }));
+        }
     }
 
     handleStopwatch = () => {
-        this.setState({
-            isRunning: !this.state.isRunning
-        })
+        this.setState(prevState => ({
+            isRunning: !prevState.isRunning
+        }));
+        if (!this.state.isRunning) {
+            this.setState({ previousTime: Date.now() });
+        }
+    }
+
+    handleReset = () => {
+        this.setState({elapsedTime: 0})
     }
 
     render() {
+
+        const seconds = Math.floor(this.state.elapsedTime/1000)
+
         return (
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
-                <span className="stopwatch-time">0</span>
-                    <button onClick={ this.handleStopwatch }>
-                        { this.state.isRunning ? 'Stop' : 'Start' }
-                    </button>
-                <button>Reset</button>
+                <span className="stopwatch-time">{seconds}</span>
+                <button onClick={this.handleStopwatch}>
+                    {this.state.isRunning ? 'Stop' : 'Start'}
+                </button>
+                <button onClick={ this.handleReset }>Reset</button>
             </div>
         )
     }
