899c47682f3cf557c1c866ad91b25c45a01bb40a
Saul Verde <saulverdemartinez@gmail.com>
2020-07-01 10:39:08 +0100

    added consumer on stats

--- scoreboard/src/components/Stats.js
+++ scoreboard/src/components/Stats.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from './Context';
 
-const Stats = ({players, highestScore}) => {
+const Stats = ({ players, highestScore }) => {
 
     const totalPlayers = players.length;
     const totalPoints = players.reduce((total, player) => {
@@ -9,22 +10,30 @@ const Stats = ({players, highestScore}) => {
     }, 0);
 
     return (
-        <table className="stats">
-            <tbody>
-                <tr>
-                    <td>Players:</td>
-                    <td>{totalPlayers}</td>
-                </tr>
-                <tr>
-                    <td>Total Points:</td>
-                    <td>{totalPoints}</td>
-                </tr>
-                <tr>
-                    <td>Highest Score:</td>
-                    <td>{highestScore}</td>
-                </tr>
-            </tbody>
-        </table>
+        <Consumer>
+            {
+                context => {
+                    return (
+                        <table className="stats">
+                            <tbody>
+                                <tr>
+                                    <td>Players:</td>
+                                    <td>{totalPlayers}</td>
+                                </tr>
+                                <tr>
+                                    <td>Total Points:</td>
+                                    <td>{totalPoints}</td>
+                                </tr>
+                                <tr>
+                                    <td>Highest Score:</td>
+                                    <td>{highestScore}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    );
+                }
+            }
+        </Consumer>
     )
 }
 
