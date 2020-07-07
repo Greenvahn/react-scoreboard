e09d93f152eca8778b77d673d820b6162a2fa2e1
Saul Verde <saulverdemartinez@gmail.com>
2020-07-07 11:49:50 +0100

    Refactor consumer props #comment pointed the props to the provider on the root app

--- scoreboard/src/components/Player.js
+++ scoreboard/src/components/Player.js
@@ -7,25 +7,24 @@ class Player extends PureComponent {
 
   static propTypes = {
     name: PropTypes.string.isRequired,
-    score: PropTypes.number.isRequired,
     index: PropTypes.number,
     id: PropTypes.number
   }
 
   render(props) {
-    const { name, score, index, id, highScore } = this.props
+    const { index } = this.props
     return (
       <div className="player">
         <Consumer>
           { 
-            context => (
+            ({ actions, players }) => (
               <span className="player-name">
-              <button className="remove-player" onClick={() => context.actions.removePlayer(id)}>✖</button>
-              <svg viewBox="0 0 44 35" className={score >= highScore && score > 0 ? 'is-high-score' : ''}>
+              <button className="remove-player" onClick={() => actions.removePlayer(players[index].id)}>✖</button>
+              <svg viewBox="0 0 44 35" className={players[index].score >= players.highScore && players[index].score > 0 ? 'is-high-score' : ''}>
                 <path d="M26.7616 10.6207L21.8192 0L16.9973 10.5603C15.3699 14.1207 10.9096 15.2672 7.77534 12.9741L0 7.24138L6.56986 28.8448H37.0685L43.5781 7.72414L35.7425 13.0948C32.6685 15.2672 28.3288 14.0603 26.7616 10.6207Z" transform="translate(0 0.301727)" />
                 <rect width="30.4986" height="3.07759" transform="translate(6.56987 31.5603)" />
               </svg>
-              {name}
+              { players[index].name }
             </span>
             )
           }
@@ -34,8 +33,6 @@ class Player extends PureComponent {
 
 
         <Counter
-          score={score}
-          highScore={highScore}
           index={index}
         />
       </div>
